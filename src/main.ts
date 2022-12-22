1ad6c1a0fabb46901df70b335cac3c3de5a85cba
AspectUnk <aspectunk@mail.ru>
2022-12-22 15:05:29 +0300

    fix

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,8 @@ import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const configService=app.get(ConfigService)
-  const port= configService.get('port')
+  const configService = app.get(ConfigService)
+  const port = configService.get('port')
   await app.listen(port);
 }
 bootstrap();
